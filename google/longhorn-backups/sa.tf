42b725c3c8a1f178e12eaa2b2506e262194997a1
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-07-01 22:41:52 +0000

    Add display name to Longhorn Backups service account

--- google/longhorn-backups/sa.tf
+++ google/longhorn-backups/sa.tf
@@ -1,6 +1,7 @@
 module "service_accounts" {
-  source     = "terraform-google-modules/service-accounts/google"
-  version    = "~> 4.0"
-  project_id = var.project_id
-  names      = ["longhorn-backups"]
+  source       = "terraform-google-modules/service-accounts/google"
+  version      = "~> 4.0"
+  project_id   = var.project_id
+  names        = ["longhorn-backups"]
+  display_name = "Longhorn Backups Service Account"
 }
